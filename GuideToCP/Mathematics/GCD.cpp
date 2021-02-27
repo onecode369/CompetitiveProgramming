42d98a84ba8cdc2ca151272999ff0350020243cd
Sanskrit Coder <sanskritcoder@gmail.com>
2021-02-27 09:21:21 +0530

    GCD of two numbers
    Signed-off-by: Sanskrit Coder <sanskritcoder@gmail.com>

---
+++ GuideToCP/Mathematics/GCD.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+#define DEBUG(x) cerr << #x << " = " << x << endl
+#define MOD 1000000007LL
+#define INF INT_MAX
+#define reset(arr,n,i) fill(arr, arr+n, i)
+#define iseql(a,b) ((abs(a-b) - 1e-9 < 0) ? 1 : 0)
+
+using namespace std;
+
+int gcd(int a,int b){
+    if(b == 0)  return a;
+    return gcd(b,a%b);
+}
+
+int main(){
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+    #endif
+    ios::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+
+    int a,b;
+    cin >> a >> b;
+    cout << gcd(a,b);
+
+    return 0;
+}
